0fca92cbeffd194f02c6d711253d6a0bcdb5a7db
akghosh111 <ghoshkiran2017@gmail.com>
2025-02-16 19:42:41 +0530

    added error middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 
 
@@ -14,6 +15,8 @@ const app = express();
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 
 
